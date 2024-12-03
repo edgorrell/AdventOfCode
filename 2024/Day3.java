3a73e72c9c0dd86bd0f8db04199e9f3110c6ceae
edgorrell <edgorrell@stu.naperville203.org>
2024-12-02 23:19:21 -0600

    LETS GO day3
    learned how to do java regex, it wasnt too bad, just had to figure out what the regex needed to be
    also because i havent done any regex before, this was cool

---
+++ 2024/Day3.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.regex.*;
+import java.io.*;
+
+public class Day3{
+    public static void main(String[] args) throws IOException{
+        Scanner scan = new Scanner(new File("day3.txt"));
+        ArrayList<String> list = new ArrayList<String>();
+        String line, str, rex;
+        int num1, num2, total = 0;
+        boolean check;
+        
+        line = scan.nextLine();
+        rex = "mul\\([0-9]+,[0-9]+\\)|do\\(\\)|don't\\(\\)";
+        Matcher m = Pattern.compile(rex).matcher(line);
+        while(m.find()){
+            list.add(m.group());
+            System.out.println(list.get(list.size()-1));
+        }
+        check = true;
+        for(int i = 0; i < list.size(); i++){
+            str = list.get(i);
+            if(str.equals("do()")){
+                check = true;
+                continue;
+            }
+            if(str.equals("don't()")){
+                check = false;
+                continue;
+            }
+            if(check){
+                num1 = Integer.parseInt(str.substring(str.indexOf("(") + 1, str.indexOf(",")));
+                num2 = Integer.parseInt(str.substring(str.indexOf(",") + 1, str.indexOf(")")));
+                total += num1 * num2;
+                System.out.println(str + ": " + num1 + ", " + num2);
+            }
+        }
+        
+        System.out.println(total);
+    }
+}
