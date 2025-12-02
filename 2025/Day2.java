ad5f89e9dec672a812922a80353c0572bdba5582
Evan Gorrell <111461168+edgorrell@users.noreply.github.com>
2025-12-01 23:35:33 -0600

    day2 lmao
    turns out im illiterate and accidentally tried to do part 2 first
    turns out pt2 is not pt1, but pt2 did work first try afterward
    maybe ill decide to be literate tomorrow

---
+++ 2025/Day2.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Day2 {
+    static ArrayList<Long> list;
+    static String line, range;
+    static long total = 0;
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(new File("2025/input.txt"));
+        list = new ArrayList<Long>();
+
+        line = scan.nextLine();
+
+        while (line.contains(",")) {
+            range = line.substring(0, line.indexOf(","));
+            line = line.substring(line.indexOf(",") + 1, line.length());
+
+            findInvalid(range);
+        }
+
+        range = line;
+        findInvalid(range);
+
+        System.out.println("-----");
+        System.out.println(total);
+        scan.close();
+    }
+
+    static void findInvalid(String range){
+        long min = Long.parseLong(range.substring(0, range.indexOf("-")));
+        long max = Long.parseLong(range.substring(range.indexOf("-") + 1, range.length()));
+
+        for (long num = min; num < max + 1; num++) {
+            String str = "" + num;
+
+            for(int length = 1; length < str.length(); length++){
+                if(str.length() % length != 0){continue;}
+
+                boolean check = true;
+                for(int pos = 0; pos < str.length()/length; pos++){
+                    if(!str.substring(length*pos,length*(pos+1)).equals(str.substring(0,length))){
+                        check = false;
+                    }
+                }
+
+                if(check && !list.contains(new Long(num))){
+                    total += num;
+                    list.add(num);
+                    System.out.println(min + "|" + max + ":" + num);
+                }
+            }
+        }
+    }
+}
